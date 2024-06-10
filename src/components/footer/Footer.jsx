484671cd4b3186183eb381df8dc3bab662aa403a
alok14598 <alok14598@gmail.com>
2024-06-10 11:38:18 +0530

    first commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,14 @@
+import styles from "./footer.module.css";
+
+const Footer = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>Alok Verma</div>
+      <div className={styles.text}>
+       
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
